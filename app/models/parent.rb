c3fe690740148a9104ef67acb5602c8586e46abe
RizwanBhatti200 <rizwanqaiserbhatti200@gmail.com>
2020-11-02 18:51:14 +0500

    add latter opner and enable confirmable

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,6 +1,14 @@
 class Parent < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # after_create :send_email
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable
+  # validates  :email, uniqueness: true, presence: true
+
+  # def send_email
+  #   ParentMailer.delay.thank_you(self)
+
+  # end
 end
