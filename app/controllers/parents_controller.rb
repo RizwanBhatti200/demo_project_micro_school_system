6c3dba5df7c321a5574448f672e64d2d3d347490
RizwanBhatti200 <rizwanqaiserbhatti200@gmail.com>
2020-11-02 18:52:03 +0500

    remove bootstrap link and add controller actions

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -1,4 +1,74 @@
 class ParentsController < ApplicationController
-  def home
+  before_action :set_parent, only: %i[show edit update destroy]
+
+  # GET /users
+  # GET /users.json
+  def index
+    @parent = Parent.all
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show; end
+
+  # GET /users/new
+  def new
+    @parent = Parent.new
+  end
+
+  # GET /users/1/edit
+  def edit; end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @parent = Parent.new(user_params)
+
+    respond_to do |format|
+      if @parent.save
+
+        format.html { redirect_to @parent, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @parent }
+      else
+        format.html { render :new }
+        format.json { render json: @parent.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      if @parent.update(user_params)
+        format.html { redirect_to @parent, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @parent }
+      else
+        format.html { render :edit }
+        format.json { render json: @parent.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @parent.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @parent = Parent.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:parent).permit(:email)
   end
 end
