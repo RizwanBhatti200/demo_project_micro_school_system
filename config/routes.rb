06d7711a94e5165eb7fcab6f9072485181916465
RizwanBhatti200 <rizwanqaiserbhatti200@gmail.com>
2020-11-03 14:38:17 +0500

    add student nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  resources :students
   get 'pods/index'
   devise_for :parents
-  
-   root to: 'pods#index'
-   
-   resources :parents, only: [:edit, :show, :update]
-
+  root to: 'parents#show'
+  resources :parents, only: [:edit, :show, :update] do
+    resources :students
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
