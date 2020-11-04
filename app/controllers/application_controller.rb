47b388a3a2bded3af50cc6a513366bd55f681b9c
RizwanBhatti200 <rizwanqaiserbhatti200@gmail.com>
2020-11-04 12:13:35 +0500

    save data from custom feilds and add views on the sing_up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_parent!
+    protect_from_forgery with: :exception
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+         def configure_permitted_parameters
+
+              devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password , :first_name,:last_name, :address])
+
+              devise_parameter_sanitizer.permit(:account_update, keys: [:first_name,:last_name, :address])
+         end
+
 end
 
