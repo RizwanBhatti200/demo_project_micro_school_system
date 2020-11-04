a4ad45dd2c00738dcb0c3c9e739a4b788691e0cb
RizwanBhatti200 <rizwanqaiserbhatti200@gmail.com>
2020-11-04 17:37:12 +0500

    edit optional Association for teacher and pods and add after save validation

--- app/models/pod.rb
+++ app/models/pod.rb
@@ -1,5 +1,11 @@
 class Pod < ApplicationRecord
+    before_create :assign_teacher
+
     belongs_to :parent
     has_many :students
-    belongs_to :teacher
+    belongs_to :teacher, optional: true
+    def assign_teacher
+        self.teacher = Teacher.all.sample if self.teacher.blank?
+    end
+    
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ApplicationRecord
   belongs_to :parent
-  belongs_to :pod
-  enum gender: { male: 0, female: 1}
+  belongs_to :pod, optional: true
+  enum gender: { male: 0, female: 1, no_gender: 3 }
 end
