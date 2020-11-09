eac3252eb89b9f764238e0bb6b6257016c8fa625
RizwanBhatti200 <rizwanqaiserbhatti200@gmail.com>
2020-11-09 18:56:51 +0500

    fixed db problems

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 5.times do |i|
-    Teacher.find_or_create_by(first_name: "Alpha#{i}", last_name: "Beta#{i}", gender:"Mail",grade:"A#{i}")
+    Teacher.find_or_create_by(first_name: "Alpha#{i}", last_name: "Beta#{i}", gender:"Mail",grade:[1,2])
   end
