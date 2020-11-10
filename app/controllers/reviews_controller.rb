dbfe26f0c9e7e08068aa9f1285e7a7df6c2501c7
RizwanBhatti200 <rizwanqaiserbhatti200@gmail.com>
2020-11-10 13:56:43 +0500

    edit contoller and add form partial

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,30 @@
 class ReviewsController < ApplicationController
+before_action :find_pod
+    
+
+ def new
+     @review = Review.new
+ end
+
+ def create
+     @object = Review.new(review_params)
+     @review.pod_id = @pod.id
+     @review.parent_id = current_parent.id
+
+     if @review.save
+        redirect_to pod_path(@pod)
+    else
+        render 'new'
+    end
+ end
+ 
+ private
+    def review_params
+        params.require(:review).permit(:rating, :comment)
+    end
+
+    def find_pod
+        @pod = Pod.find(params[:pod_id])
+    end
+ 
 end
