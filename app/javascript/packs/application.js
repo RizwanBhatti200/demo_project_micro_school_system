1e581764d843debca3ee9a3512902eec0a73342e
RizwanBhatti200 <rizwanqaiserbhatti200@gmail.com>
2020-11-21 16:45:10 +0500

    wip about webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,15 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-require("@rails/ujs").start();
-require("turbolinks").start();
+import 'jquery';
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("stylesheet/application.scss").start()
+require("packs/custom")
+require("packs/jquery.raty")
+require("packs/popup")
 
-require("stylesheet/application.scss")
-//
+
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
